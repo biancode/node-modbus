b2cbbf215bd40e865f255a9d062482b3f0e61d52
Klaus Landsdorf <klaus@bianco-royal.de>
2018-05-22 18:31:02 +0200

    fix(server): issue #16 fix for empty messages

--- src/modbus-server-core.js
+++ src/modbus-server-core.js
@@ -49,31 +49,31 @@ let core = stampit()
     this.onData = function (pdu, callback) {
       this.log.debug('incoming data')
 
-      // get fc and byteCount in advance
-      let fc = pdu.readUInt8(0)
+      let reqHandler, fc
+      if (pdu.length && pdu.length > 0) {
+        // get fc and byteCount in advance
+        fc = pdu.readUInt8(0)
+        // get the pdu handler
+        reqHandler = handler[fc]
+        if (!reqHandler) {
+          // write a error/exception pkt to the
+          // socket with error code fc + 0x80 and
+          // exception code 0x01 (Illegal Function)
+          this.log.debug('no handler for fc', fc)
 
-      // get the pdu handler
-      let reqHandler = handler[fc]
+          let buf = Buffer.alloc(2)
+          buf.writeUInt8(fc + 0x80, 0)
+          buf.writeUInt8(0x01, 1)
 
-      if (!reqHandler) {
-        // write a error/exception pkt to the
-        // socket with error code fc + 0x80 and
-        // exception code 0x01 (Illegal Function)
-
-        this.log.debug('no handler for fc', fc)
-
-        let buf = Buffer.alloc(2)
-        buf.writeUInt8(fc + 0x80, 0)
-        buf.writeUInt8(0x01, 1)
-
-        callback(buf)
-
-        return
+          callback(buf)
+        } else {
+          reqHandler(pdu, function (response) {
+            callback(response)
+          })
+        }
+      } else {
+        this.log.debug('nothing to do - got an empty message without fc')
       }
-
-      reqHandler(pdu, function (response) {
-        callback(response)
-      })
     }.bind(this)
 
     this.setRequestHandler = function (fc, callback) {
